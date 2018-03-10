801609258ed9f300a446d610b70248ad9e565a4c
Carter Bray <carter.bray@gmail.com>
2018-03-10 12:33:36 -0800

    Refactor parser, handle operator precedence

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,6 +6,7 @@
 #include <cassert>
 
 namespace algebra {
+namespace parser {
 
 //------------------------------------------------------------------------------
 struct token
@@ -30,8 +31,30 @@ struct token
     }
 };
 
+std::string operator+(char const* lhs, token const& rhs)
+{
+    return std::string(lhs) + std::string(rhs.begin, rhs.end);
+}
+
+std::string operator+(std::string const& lhs, token const& rhs)
+{
+    return lhs + std::string(rhs.begin, rhs.end);
+}
+
 //------------------------------------------------------------------------------
-std::vector<token> tokenize(char const* str)
+struct error
+{
+    token tok;
+    std::string msg;
+};
+
+template<typename T> using result = std::variant<error, T>;
+
+//------------------------------------------------------------------------------
+using tokenized = std::vector<token>;
+
+//------------------------------------------------------------------------------
+result<tokenized> tokenize(char const* str)
 {
     std::vector<token> tokens;
     while (true) {
@@ -63,6 +86,9 @@ std::vector<token> tokenize(char const* str)
             bool has_dot = false;
             while (*str >= '0' && *str <= '9' || (!has_dot && *str == '.')) {
                 if (*str == '.') {
+                    if (has_dot) {
+                        return error{{str, str+1}, "invalid literal"};
+                    }
                     has_dot = true;
                 }
                 ++t.end;
@@ -80,152 +106,286 @@ std::vector<token> tokenize(char const* str)
             tokens.push_back(t);
             continue;
         }
+
+        return error{{str, str+1}, "invalid character '" + token{str, str+1} + "'"};
     }
 
     // never gets here
 }
 
 //------------------------------------------------------------------------------
-expression parse(token const*& tokens, token const* end);
+result<expression> parse_expression(token const*& tokens, token const* end);
+result<expression> parse_operand(token const*& tokens, token const* end);
 
 //------------------------------------------------------------------------------
-expression parse_binary_fn(op_type fn, token const*& tokens, token const* end)
+result<op_type> parse_operator(token const*& tokens, token const* end)
 {
-    assert(*tokens == '(');
-    expression lhs = parse(++tokens, end);
-    assert(*tokens == ',');
-    expression rhs = parse(++tokens, end);
-    assert(*tokens == ')');
-    ++tokens;
-    return op{fn, lhs, rhs};
-}
+    if (tokens >= end) {
+        return error{*(tokens - 1), "expected operator after '" + *(tokens - 1) + "'"};
+    } else if (*tokens == ')' || *tokens == ',') {
+        return error{*tokens, "expected operator after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+    }
 
-//------------------------------------------------------------------------------
-expression parse_unary_fn(op_type fn, token const*& tokens, token const* end)
-{
-    assert(*tokens == '(');
-    expression lhs = parse(++tokens, end);
-    assert(*tokens == ')');
-    ++tokens;
-    return op{fn, lhs};
-}
-
-//------------------------------------------------------------------------------
-expression parse(token const*& tokens, token const* end)
-{
-    expression lhs = empty{};
-
-    if (tokens[0] == '(') {
-        lhs = parse(++tokens, end);
-        assert(*tokens == ')');
+    if (tokens[0] == '+') {
         ++tokens;
+        return op_type::sum;
+    } else if (tokens[0] == '-') {
+        ++tokens;
+        return op_type::difference;
+    } else if (tokens[0] == '*') {
+        ++tokens;
+        return op_type::product;
+    } else if (tokens[0] == '/') {
+        ++tokens;
+        return op_type::quotient;
+    } else if (tokens[0] == '^') {
+        ++tokens;
+        return op_type::exponent;
+    } else {
+        return error{*tokens, "expected operator, found '" + *tokens + "'"};
+    }
+}
+
+//------------------------------------------------------------------------------
+result<expression> parse_binary_function(token const*& tokens, token const* end, op_type type)
+{
+    if (tokens >= end) {
+        return error{*(tokens - 1), "expected '(' after '" + *(tokens - 1) + "'"};
+    } else if (*tokens != '(') {
+        return error{*tokens, "expected '(' after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+    } else {
+        ++tokens;
+    }
+
+    result<expression> lhs = parse_operand(tokens, end);
+    if (std::holds_alternative<error>(lhs)) {
+        return std::get<error>(lhs);
+    }
+
+    if (tokens >= end) {
+        return error{*(tokens - 1), "expected ',' after '" + *(tokens - 1) + "'"};
+    } else if (*tokens != ',') {
+        return error{*tokens, "expected ',' after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+    } else {
+        ++tokens;
+    }
+
+    result<expression> rhs = parse_operand(tokens, end);
+    if (std::holds_alternative<error>(rhs)) {
+        return std::get<error>(rhs);
+    }
+
+    if (tokens >= end) {
+        return error{*(tokens - 1), "expected ')' after '" + *(tokens - 1) + "'"};
+    } else if (*tokens != ')') {
+        return error{*tokens, "expected ')' after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+    } else {
+        ++tokens;
+    }
+
+    return op{type, std::get<expression>(lhs), std::get<expression>(rhs)};
+}
+
+//------------------------------------------------------------------------------
+result<expression> parse_unary_function(token const*& tokens, token const* end, op_type type, expression const& rhs = empty{})
+{
+    result<expression> arg = parse_operand(tokens, end);
+    if (std::holds_alternative<error>(arg)) {
+        return std::get<error>(arg);
+    }
+    return op{type, std::get<expression>(arg), rhs};
+}
+
+//------------------------------------------------------------------------------
+result<expression> parse_operand(token const*& tokens, token const* end)
+{
+    if (tokens >= end) {
+        return error{*(tokens - 1), "expected expression after '" + *(tokens - 1) + "'"};
+    } else if (*tokens == ')' || *tokens == ',') {
+        return error{*tokens, "expected expression after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+    }
+
+    if (*tokens == '(') {
+        result<expression> expr = parse_expression(++tokens, end);
+        if (std::holds_alternative<error>(expr)) {
+            return std::get<error>(expr);
+        }
+        if (tokens >= end) {
+            return error{*(tokens - 1), "expected ')' after '" + *(tokens - 1) + "'"};
+        } else if (*tokens != ')') {
+            return error{*tokens, "expected ')' after '" + *(tokens - 1) + "', found '" + *tokens + "'"};
+        }
+        ++tokens;
+        return std::get<expression>(expr);
 
     //
     //  functions
     //
 
-    } else if (tokens[0] == "ln") {
+    } else if (*tokens == "ln") {
+        return parse_unary_function(++tokens, end, op_type::logarithm, constant::e);
+    } else if (*tokens == "log") {
+        return parse_binary_function(++tokens, end, op_type::logarithm);
+    } else if (*tokens == "sin") {
+        return parse_unary_function(++tokens, end, op_type::sine);
+    } else if (*tokens == "cos") {
+        return parse_unary_function(++tokens, end, op_type::cosine);
+    } else if (*tokens == "tan") {
+        return parse_unary_function(++tokens, end, op_type::tangent);
+    } else if (*tokens == "sec") {
+        return parse_unary_function(++tokens, end, op_type::secant);
+    } else if (*tokens == "csc") {
+        return parse_unary_function(++tokens, end, op_type::cosecant);
+    } else if (*tokens == "cot") {
+        return parse_unary_function(++tokens, end, op_type::cotangent);
+
+    //
+    //  constants
+    //
+
+    } else if (*tokens == '0') {
         ++tokens;
-        assert(*tokens == '(');
-        lhs = parse(++tokens, end);
-        assert(*tokens == ')');
+        return constant::zero;
+    } else if (tokens[0] == '1') {
         ++tokens;
-        lhs = op{op_type::logarithm, lhs, constant::e};
-    } else if (tokens[0] == "log") {
-        lhs = parse_binary_fn(op_type::logarithm, ++tokens, end);
-    } else if (tokens[0] == "sin") {
-        lhs = parse_unary_fn(op_type::sine, ++tokens, end);
-    } else if (tokens[0] == "cos") {
-        lhs = parse_unary_fn(op_type::cosine, ++tokens, end);
-    } else if (tokens[0] == "tan") {
-        lhs = parse_unary_fn(op_type::tangent, ++tokens, end);
-    } else if (tokens[0] == "sec") {
-        lhs = parse_unary_fn(op_type::secant, ++tokens, end);
-    } else if (tokens[0] == "csc") {
-        lhs = parse_unary_fn(op_type::cosecant, ++tokens, end);
-    } else if (tokens[0] == "cot") {
-        lhs = parse_unary_fn(op_type::cotangent, ++tokens, end);
+        return constant::one;
+    } else if (tokens[0] == '2') {
+        ++tokens;
+        return constant::two;
+    } else if (tokens[0] == "pi") {
+        ++tokens;
+        return constant::pi;
+    } else if (tokens[0] == 'e') {
+        ++tokens;
+        return constant::e;
+    } else if (tokens[0] == 'i') {
+        ++tokens;
+        return constant::i;
+
+    //
+    //  values
+    //
+
+    } else if (*tokens[0].begin >= '0' && *tokens[0].begin <= '9' || *tokens[0].begin == '.') {
+        value v = std::atof(tokens[0].begin);
+        ++tokens;
+        return v;
+
+    //
+    //  symbols
+    //
+
+    } else if (*tokens[0].begin >= 'a' && *tokens[0].begin <= 'z' || *tokens[0].begin >= 'A' && *tokens[0].begin <= 'Z') {
+        symbol s{tokens[0].begin, tokens[0].end};
+        ++tokens;
+        return s;
+
+    } else {
+        return error{*tokens, "syntax error: '" + *tokens + "'"};
+    }
+}
+
+//------------------------------------------------------------------------------
+constexpr int precedence(op_type t)
+{
+    switch (t) {
+        case op_type::sum: return 6;
+        case op_type::difference: return 6;
+        case op_type::product: return 5;
+        case op_type::quotient: return 5;
+        case op_type::exponent: return 4;
+        default: return 0;
+    }
+}
+
+//------------------------------------------------------------------------------
+result<expression> parse_expression(token const*& tokens, token const* end)
+{
+    result<expression> lhs = parse_operand(tokens, end);
+    if (std::holds_alternative<error>(lhs)) {
+        return std::get<error>(lhs);
     }
 
-    while (tokens < end && tokens[0] != ')' && tokens[0] != ',') {
+    // single operand, return immediately
+    if (tokens == end || *tokens == ')' || *tokens == ',') {
+        return std::get<expression>(lhs);
+    }
 
-        //
-        //  constants
-        //
+    result<op_type> lhs_op = parse_operator(tokens, end);
+    if (std::holds_alternative<error>(lhs_op)) {
+        return std::get<error>(lhs_op);
+    }
 
-        if (tokens[0] == '0') {
-            ++tokens;
-            lhs = constant::zero;
-            continue;
-        } else if (tokens[0] == '1') {
-            ++tokens;
-            lhs = constant::one;
-            continue;
-        } else if (tokens[0] == '2') {
-            ++tokens;
-            lhs = constant::two;
-            continue;
-        } else if (tokens[0] == "pi") {
-            ++tokens;
-            lhs = constant::pi;
-            continue;
-        } else if (tokens[0] == 'e') {
-            ++tokens;
-            lhs = constant::e;
-            continue;
-        } else if (tokens[0] == 'i') {
-            ++tokens;
-            lhs = constant::i;
-            continue;
+    result<expression> mid = parse_operand(tokens, end);
+    if (std::holds_alternative<error>(mid)) {
+        return std::get<error>(mid);
+    }
+
+    // combine operands account for operator precedence
+    while (tokens < end) {
+        if (*tokens == ')' || *tokens == ',') {
+            break;
         }
 
-        //
-        //  values
-        //
-
-        if (*tokens[0].begin >= '0' && *tokens[0].begin <= '9' || *tokens[0].begin == '.') {
-            lhs = std::atof(tokens[0].begin);
-            ++tokens;
-            continue;
+        result<op_type> rhs_op = parse_operator(tokens, end);
+        if (std::holds_alternative<error>(rhs_op)) {
+            return std::get<error>(rhs_op);
         }
 
-        //
-        //  symbols
-        //
-
-        if (*tokens[0].begin >= 'a' && *tokens[0].begin <= 'z' || *tokens[0].begin >= 'A' && *tokens[0].begin <= 'Z') {
-            lhs = symbol{tokens[0].begin, tokens[0].end};
-            ++tokens;
-            continue;
+        result<expression> rhs = parse_operand(tokens, end);
+        if (std::holds_alternative<error>(rhs)) {
+            return std::get<error>(rhs);
         }
 
-        //
-        //  operators
-        //
+        //  (lhs op mid) op rhs         lhs op (mid op rhs)
+        //       v           v    or     v          v
+        //      lhs         mid         lhs        mid
 
-        if (tokens[0] == '+') {
-            return op{op_type::sum, lhs, parse(++tokens, end)};
-        } else if (tokens[0] == '-') {
-            return op{op_type::difference, lhs, parse(++tokens, end)};
-        } else if (tokens[0] == '*') {
-            return op{op_type::product, lhs, parse(++tokens, end)};
-        } else if (tokens[0] == '/') {
-            return op{op_type::quotient, lhs, parse(++tokens, end)};
-        } else if (tokens[0] == '^') {
-            return op{op_type::exponent, lhs, parse(++tokens, end)};
+        if (precedence(std::get<op_type>(lhs_op)) <= precedence(std::get<op_type>(rhs_op))) {
+            lhs = op{std::get<op_type>(lhs_op), std::get<expression>(lhs), std::get<expression>(mid)};
+            lhs_op = rhs_op;
+            mid = rhs;
+        } else {
+            mid = op{std::get<op_type>(rhs_op), std::get<expression>(mid), std::get<expression>(rhs)};
         }
     }
 
-    return lhs;
+    return op{std::get<op_type>(lhs_op), std::get<expression>(lhs), std::get<expression>(mid)};
+}
+
+} // namespace parser
+
+//------------------------------------------------------------------------------
+void print_error(char const* str, parser::error const& err)
+{
+    std::ptrdiff_t offset = err.tok.begin - str;
+    std::ptrdiff_t length = err.tok.end - err.tok.begin;
+    for (std::ptrdiff_t ii = 0; ii < offset; ++ii) {
+        printf(" ");
+    }
+    for (std::ptrdiff_t ii = 0; ii < length; ++ii) {
+        printf("^");
+    }
+    printf(" %s\n", err.msg.c_str());
 }
 
 //------------------------------------------------------------------------------
 expression parse(char const* str)
 {
-    std::vector<token> tokens = tokenize(str);
-    token const* begin = &tokens[0];
-    token const* end = begin + tokens.size();
-    return parse(begin, end);
+    parser::result<parser::tokenized> result = parser::tokenize(str);
+    if (std::holds_alternative<parser::tokenized>(result)) {
+        parser::tokenized const& tokens = std::get<parser::tokenized>(result);
+        parser::token const* begin = &tokens[0];
+        parser::result<expression> expr = parser::parse_expression(begin, begin + tokens.size());
+        if (std::holds_alternative<parser::error>(expr)) {
+            print_error(str, std::get<parser::error>(expr));
+        } else {
+            return std::get<expression>(expr);
+        }
+    } else if (std::holds_alternative<parser::error>(result)) {
+        print_error(str, std::get<parser::error>(result));
+    }
+    return expression{};
 }
 
 } // namespace algebra
